89df1e52c2dec1ae2bfe5b9f6795d7a63fef8ce6
antonzaratustra <antonzaratustra@gmail.com>
2023-05-11 11:51:27 +0300

    create like as function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
-"use strict";
+ "use strict";
 
-const slides = document.querySelectorAll(".slide");
+function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll(".slide");
 
-for(const slide of slides) {
-    slide.addEventListener ('click', () => {
-        clearActiveClasses();
-        slide.classList.add('active');
-    })
+    slides[activeSlide].classList.add('active');
+    
+    for(const slide of slides) {
+        slide.addEventListener ('click', () => {
+            clearActiveClasses();
+            slide.classList.add('active');
+        })
+    }
+    
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        })
+    }
 }
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    })
-}
+setTimeout(slidesPlugin, 1000, 1);
+
+
+
+
+
 
